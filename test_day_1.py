671f679e2514cb2d72efb1fc6503f7181a749282
Vemund Rogne <vemundrogne@gmail.com>
2020-12-01 16:25:08 +0100

    verify solution can now take a list of numbers

--- test_day_1.py
+++ test_day_1.py
@@ -10,18 +10,22 @@ def find_solution_brute(numbers):
                 return (number_1, number_2)
 
 
-def verify_solution(number_1, number_2):
-    assert number_1 + number_2 == 2020
-    return number_1 * number_2
+def verify_solution(numbers):
+    assert sum(numbers) == 2020
+
+    running_product = 1
+    for number in numbers:
+        running_product = running_product * number
+    return running_product
 
 
 def test_brute_solution():
-    number_1, number_2 = find_solution_brute(
+    solution = find_solution_brute(
         read_input(
             "day_1_input.txt"
         )
     )
 
-    solution = verify_solution(number_1, number_2)
+    solution = verify_solution(solution)
     assert solution == 888331
     print("Solution:", solution)
