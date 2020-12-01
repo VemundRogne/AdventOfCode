09a86d84e36b83b5e3b4958b2be8aed3432829da
Vemund Rogne <vemundrogne@gmail.com>
2020-12-01 16:16:55 +0100

    Day1_part1 complete

---
+++ test_day_1.py
@@ -0,0 +1,26 @@
+def read_input(path=str):
+    with open(path, 'r') as f:
+        return [int(line) for line in f]
+
+
+def find_solution_brute(numbers):
+    for number_1 in numbers:
+        for number_2 in numbers:
+            if number_1 + number_2 == 2020:
+                return (number_1, number_2)
+
+
+def verify_solution(number_1, number_2):
+    assert number_1 + number_2 == 2020
+    return number_1 * number_2
+
+
+def test_brute_solution():
+    number_1, number_2 = find_solution_brute(
+        read_input(
+            "day_1_input.txt"
+        )
+    )
+
+    solution = verify_solution(number_1, number_2)
+    print("Solution:", solution)
